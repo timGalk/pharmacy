56e95e7c9468a8a3a871709dd5113755cd2bb42a
timGalk <tivron@outlook.com>
2025-06-26 22:18:04 +0200

    Add ArticleEntity; implement article attributes and structure

--- src/main/java/com/edu/pharmacy/entity/ArticleEntity.java
+++ src/main/java/com/edu/pharmacy/entity/ArticleEntity.java
@@ -7,22 +7,50 @@ import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
 
+
+/**
+ * Represents an article entity in the system.
+ * This entity is mapped to the "articles" table in the database.
+ */
 @Entity
 @Table(name = "articles")
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@AllArgsConstructor // Generates a constructor with all fields as parameters.
+@NoArgsConstructor  // Generates a no-argument constructor.
+@Builder            // Enables the builder pattern for creating instances of this class.
 public class ArticleEntity {
+
+    /**
+     * The unique identifier for the article.
+     * This field is auto-generated using the IDENTITY strategy.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long  id;
+    private Long id;
+
+    /**
+     * The title of the article.
+     */
     private String title;
+
+    /**
+     * A collection of tags associated with the article.
+     * This is stored as an element collection in the database.
+     */
     @ElementCollection
     private HashSet<String> tags;
 
-    private int timeToRead; // in minutes
+    /**
+     * The estimated time to read the article, in minutes.
+     */
+    private int timeToRead;
+
+    /**
+     * The content of the article.
+     */
     private String content;
+
+    /**
+     * The author of the article.
+     */
     private String author;
-
-
 }
