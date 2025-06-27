3d895ce4739ba546f6923eb6a20fa9207e807d0c
timGalk <tivron@outlook.com>
2025-06-27 02:42:18 +0200

    Update user retrieval endpoint to use path variable for user ID

--- src/main/java/com/edu/pharmacy/controller/UserController.java
+++ src/main/java/com/edu/pharmacy/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping(value = "/id")
+    @GetMapping(value = "/{id}")
     public UserDTO getUser(@PathVariable Long id) {
         return userService.getUser(id);
     }
